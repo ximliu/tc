7d20bf032dcabb858437647f61eb232f753d9416
chendong12 <43974374+chendong12@users.noreply.github.com>
2019-11-12 23:54:31 +0800

    Update table15_route_check.sh

--- router/table15_route_check.sh
+++ router/table15_route_check.sh
@@ -5,7 +5,7 @@ route_table15=`/usr/sbin/ip route show table 15`
 route_length15=${#route_table15}
 if [ $route_length15 == 0 ]
 then
-   echo -e "$DATE $Client Table 15 Server 路由丢失" >> /var/log/VpnServer.log
+   echo -e "$DATE $Client Table 15 特殊国内路由丢失" >> /var/log/VpnServer.log
    /root/dst_noVPN_route.sh
 else
    echo "Table 15 路由正常"
