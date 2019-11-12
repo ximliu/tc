7afeb1f3f13ba014e892bbfc600321810b93f600
chendong12 <43974374+chendong12@users.noreply.github.com>
2019-11-12 15:09:47 +0800

    Update dst_noVPN_route.sh

--- router/dst_noVPN_route.sh
+++ router/dst_noVPN_route.sh
@@ -10,3 +10,15 @@ ip route add 13.96.0.0/13 $OLDGW table 15
 ip route add 13.104.0.0/14 $OLDGW table 15
 #微软自动更新
 ip route add 13.107.4.50/32 $OLDGW table 15
+
+#epcchem.com 企业邮箱
+ip route add 54.86.81.5 $OLDGW table 15
+ip route add 54.88.144.211 $OLDGW table 15
+#outlook
+ip route add 50.116.65.39 $OLDGW table 15
+ip route add 52.98.72.178 $OLDGW table 15
+#office365.com
+ip route add 40.100.0.0/16 $OLDGW table 15 
+
+#microsoft
+ip route add 52.96.0.0/12 $OLDGW table 15
