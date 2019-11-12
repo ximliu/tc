ba296531fadbf4c6112068315fc2315e38346c0d
chendong12 <43974374+chendong12@users.noreply.github.com>
2019-11-12 23:53:44 +0800

    Update china_route_check.sh

--- router/china_route_check.sh
+++ router/china_route_check.sh
@@ -5,7 +5,7 @@ route_tun1=`/usr/sbin/ip route show table 10 | grep '1.0.1.0/24'`
 route_length=${#route_tun1}
 if [ $route_length == 0 ]
 then
-   echo -e "$DATE $Client 国内路由丢失" >> /var/log/VpnServer.log
+   echo -e "$DATE $Client Table 10 国内路由丢失" >> /var/log/VpnServer.log
    /root/chnroutes/vpn-up.sh
 #else
 #   echo "路由正常"
