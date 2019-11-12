21d6c1f2f8cb05783e360b30d05aeff29ae4d484
chendong12 <43974374+chendong12@users.noreply.github.com>
2019-11-12 16:35:09 +0800

    Update alive_check_4udp2raw.sh

--- router/alive_check_4udp2raw.sh
+++ router/alive_check_4udp2raw.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#监测 udp2raw_amd64是否正常运行，不运行则自动启动
+server1=`cat /root/mutiUDP2raw.sh | grep server1= | cut -f2 -d "'"`
+server2=`cat /root/mutiUDP2raw.sh | grep server2= | cut -f2 -d "'"`
+ps1="/root/udp2raw/udp2raw_amd64 -c -r$server1:50000 -l 127.0.0.1:1198 --raw-mode faketcp -k udp2rawpassword"
+ps2="/root/udp2raw/udp2raw_amd64 -c -r$server2:50000 -l 127.0.0.1:1199 --raw-mode faketcp -k udp2rawpassword"
 
+PROC=("$ps1" "$ps2")
+for p in "${PROC[@]}"
+do
+  ps aux|grep "$p" |grep -v grep > /dev/null;
+  if [ $? -eq 0 ]; then
+    echo "`date` Process $p is running"
+  else
+    echo "`date` Process $p is not running. Prepare to start" >> /var/log/udp2raw.log
+    /root/mutiUDP2raw.sh >> /var/log/udp2raw.log
+    sleep 5
+    echo -e "`date` Client_name 定期监测udp2raw异常，设备自动重新启动udp2raw进程" | mail -s "udp2raw进程重启提醒" your_email@qq.com
+  fi
+done
