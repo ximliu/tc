ff8f871000d925e350f8eb5cbd823f648dcf5b64
chendong12 <43974374+chendong12@users.noreply.github.com>
2020-10-15 23:10:05 +0800

    Update checkstat.sh

--- server/checkstat.sh
+++ server/checkstat.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-udp2raw_count="`ps aux|grep udp2raw_amd64|grep -v 'grep'|wc -l`"
+udp2raw_count="`ps aux|grep 'udp2raw_amd64 -s'|grep -v 'grep'|wc -l`"
 if [ $udp2raw_count -ne 3 ];then
   ps -ef | grep '/root/udp2raw/udp2raw_amd64 -s' | grep -v grep | awk '{print $2}' | xargs kill -9
   /root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50000 -r 127.0.0.1:1198 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
