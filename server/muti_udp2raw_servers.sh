5028fc232534b9acc84da8b5788e889f22542eff
chendong12 <43974374+chendong12@users.noreply.github.com>
2020-10-15 23:50:04 +0800

    Update muti_udp2raw_servers.sh

--- server/muti_udp2raw_servers.sh
+++ server/muti_udp2raw_servers.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
-ps -ef | grep '/root/udp2raw/udp2raw_amd64' | grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | grep '/root/speeder/speederv2_amd64' | grep -v grep | awk '{print $2}' | xargs kill -9
-#nohup /root/speeder/speederv2_amd64 -s -l 0.0.0.0:50000 -r 127.0.0.1:1198 -f20:5 -i 8 -k huayu888 --mode 1 >/var/log/speederv2.log &
-#nohup /root/speeder/speederv2_amd64 -s -l 0.0.0.0:50001 -r 127.0.0.1:1199 -f20:5 -i 8 -k huayu888 --mode 1 >/var/log/speederv2.log &
-/root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50000 -r 127.0.0.1:1198 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
-/root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50001 -r 127.0.0.1:1199 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
-/root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50002 -r 127.0.0.1:1120 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
+systemctl start supervisord
+#ps -ef | grep '/root/udp2raw/udp2raw_amd64 -s' | grep -v grep | awk '{print $2}' | xargs kill -9
+#/root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50000 -r 127.0.0.1:1198 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
+#/root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50001 -r 127.0.0.1:1199 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
+#/root/udp2raw/udp2raw_amd64 -s -l0.0.0.0:50002 -r 127.0.0.1:1120 --raw-mode faketcp -k huayu888 --fix-gro --cipher-mode xor --auth-mode simple &
 /root/tc6M_tun0.sh
 /root/tc6M_tun1.sh
