70b081d431a799c52a5fac61d095c089933438b6
chendong12 <43974374+chendong12@users.noreply.github.com>
2020-10-22 09:08:33 +0800

    Create calc_remain_flow_count.sh

---
+++ other/calc_remain_flow_count.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rx_pre=$(cat /proc/net/dev | grep eth0 | tr : " " | awk '{print $2}')
+rx_pre_GB=`echo "scale=2;$rx_pre/1024/1024/1024" | bc`
+echo "eth0 rx flow is: $rx_pre_GB GB"
+used_flow=`echo "scale=2;$rx_pre_GB*1.2" | bc`
+echo "used_flow is 120 precent of eth0 rx flow is: $used_flow GB"
+remain_flow=`cat remain_flow`
+echo "remain flow is: $remain_flow"
+remain_flow=`echo "scale=2;$remain_flow-$used_flow" | bc`
+echo "remain flow - used flow is: $remain_flow"
+echo $remain_flow > ./remain_flow
